ddc0281c135ac5ebe7b1d81b85719878365ef54d
ALeksandr Yurkovskiy <alus03@mail.ru>
2023-05-15 08:43:11 +0300

    7 Организация стилей. Добавляем темы

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import classes from "./Counter.module.scss";
 
 export const Counter = () => {
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import { render } from "react-dom";
 import { Counter } from "./components/Counter";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./theme/ThemeProvider";
 // import React from "react";
 
 render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
