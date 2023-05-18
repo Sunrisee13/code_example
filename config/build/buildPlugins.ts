a91f7b1010a2deb5c4600766c5d4a28ba5573a3f
ALeksandr Yurkovskiy <alus03@mail.ru>
2023-05-18 12:01:12 +0300

    23 Анализ размера банда. BundleAnalyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,6 +2,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 import webpack from 'webpack'
 import { type BuildOptions } from './types/config'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 export function buildPlugins ({
   paths,
@@ -21,6 +22,9 @@ export function buildPlugins ({
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev)
+    }),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false
     })
   ]
 }
