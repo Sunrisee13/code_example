d8ed92e7f6e77d0d36e49dc79963480fdb56e2ac
ALeksandr Yurkovskiy <alus03@mail.ru>
2023-05-21 17:22:20 +0300

    31.1 Json server. Имитация бэкенд

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,9 +1,8 @@
-import { Counter } from 'entities/Counter'
 import { useTranslation } from 'react-i18next'
 
 const MainPage = () => {
   const { t } = useTranslation()
-  return <div>{t('Главная страница')}<Counter /></div>
+  return <div>{t('Главная страница')}</div>
 }
 
 export default MainPage
