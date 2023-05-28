449ce37a00c1cfdc220148bff59891b786d24595
ALeksandr Yurkovskiy <alus03@mail.ru>
2023-05-28 15:16:00 +0300

    39 Модуль профиля. Фетчинг данных. TS strict mode

---
+++ src/entities/Profile/model/services/fetchProfileData/fetchProfileData.ts
@@ -0,0 +1,23 @@
+import { createAsyncThunk } from '@reduxjs/toolkit'
+import { type ThunkConfig } from 'app/providers/StoreProvider'
+import { type Profile } from '../../types/profile'
+
+// <ТоЧтоВозвращаем, Аргумент, config>
+// eslint-disable-next-line @typescript-eslint/no-invalid-void-type
+export const fetchProfileData = createAsyncThunk<Profile, void, ThunkConfig<string>>(
+  'profile/fetchProfileData',
+  async (_, thunkAPI) => {
+    const { extra, rejectWithValue, dispatch } = thunkAPI
+    try {
+      const response = await extra.api.get<Profile>('/profile')
+
+      return response.data
+    } catch (e) {
+      console.log(e)
+      return rejectWithValue('error')
+      // Если моими словами, как я понял эту конструкцию
+      // Мы создаём экстра редьюсеры, которые зависят от состояния thunk
+      // и с помощью thunk Api мы можем передавать payload (который мы протипизировали)
+    }
+  }
+)
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -3,7 +3,7 @@ type Mods = Record<string, boolean | string>
 export function classNames (
   cls: string,
   mods: Mods = {},
-  additional: string[] = []
+  additional: Array<string | undefined> = []
 ): string {
   const result = [cls, ...additional.filter(Boolean)]
   for (const key in mods) {
