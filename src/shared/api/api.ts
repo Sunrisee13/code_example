30bd0a892e5d04bd83dd7607195ad012fe3e4834
ALeksandr Yurkovskiy <alus03@mail.ru>
2023-07-23 17:42:02 +0300

    62 CopyPlugin. Подготовка к продакшну. Публикуем на Netlify

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -3,8 +3,10 @@ import { USER_LOCALSTORAGE_KEY } from 'shared/consts/localstorage'
 
 export const $api = axios.create({
 
-  baseURL: __API__,
-  headers: {
-    authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || ''
-  }
+  baseURL: __API__
+})
+
+$api.interceptors.request.use((config) => {
+  if (config.headers) config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || ''
+  return config
 })
