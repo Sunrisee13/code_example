1dcedf868d435866eef87766af19b2b684904121
ALeksandr Yurkovskiy <alus03@mail.ru>
2023-08-27 19:05:28 +0300

    84 BrowserList. Размер бандла. Исправляем Drawer animation provid

--- src/feature/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/feature/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -8,7 +8,6 @@ import { Popover } from '@/shared/ui/Popups'
 import { Drawer } from '@/shared/ui/Drawer/Drawer'
 import { BrowserView, MobileView } from 'react-device-detect'
 import cls from './NotificationButton.module.scss'
-import { AnimationProvider } from '@/shared/lib/AnimationProvider'
 
 interface NotificationButtonProps {
   className?: string
@@ -44,11 +43,10 @@ export const NotificationButton = memo((props: NotificationButtonProps) => {
                 </Popover>
             </BrowserView>
             <MobileView>
-                {trigger} <AnimationProvider>
+                {trigger}
                     <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
                         <NotificationList />
                     </Drawer>
-                </AnimationProvider>
             </MobileView>
         </div>
 
--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -2,7 +2,7 @@ import { classNames } from '@/shared/lib/classNames/classNames'
 import React, {
   memo, type ReactNode, useCallback, useEffect
 } from 'react'
-import { useAnimationLibs } from '@/shared/lib/AnimationProvider'
+import { AnimationProvider, useAnimationLibs } from '@/shared/lib/AnimationProvider'
 import { useTheme } from '@/app/providers/ThemeProvider'
 import { Overlay } from '../Overlay/Overlay'
 import cls from './Drawer.module.scss'
@@ -96,7 +96,7 @@ export const DrawerContent = memo((props: DrawerProps) => {
   )
 })
 
-export const Drawer = memo((props: DrawerProps) => {
+const DrawerAsync = (props: DrawerProps) => {
   const { isLoaded } = useAnimationLibs()
 
   if (!isLoaded) {
@@ -104,4 +104,12 @@ export const Drawer = memo((props: DrawerProps) => {
   }
 
   return <DrawerContent {...props} />
-})
+}
+
+export const Drawer = (props: DrawerProps) => {
+  return (
+      <AnimationProvider>
+          <DrawerAsync {...props} />
+      </AnimationProvider>
+  )
+}
