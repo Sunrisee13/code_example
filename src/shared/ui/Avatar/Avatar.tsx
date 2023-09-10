346b960b0702e8d82094e8368662b5f6513297eb
ALeksandr Yurkovskiy <alus03@mail.ru>
2023-09-10 17:16:33 +0300

    106 Работа с изображенями. AppImage, Preload, lazy

--- src/shared/ui/Avatar/Avatar.tsx
+++ src/shared/ui/Avatar/Avatar.tsx
@@ -1,32 +1,46 @@
 import { type CSSProperties, useMemo } from 'react'
 
-import { classNames } from '@/shared/lib/classNames/classNames'
+import { classNames, type Mods } from '@/shared/lib/classNames/classNames'
 
 import cls from './Avatar.module.scss'
+import UserIcon from '../../assets/icons/user-filled.svg'
+import { AppImage } from '../AppImage'
+import { Icon } from '../Icon'
+import { Skeleton } from '../Skeleton'
 
 interface AvatarProps {
   className?: string
   src?: string
   size?: number
   alt?: string
+  fallbackInverted?: boolean
 }
 
-export const Avatar = (props: AvatarProps) => {
-  const {
-    className,
-    src,
-    size,
-    alt
-  } = props
-  const mods = {}
-  const styles = useMemo<CSSProperties>(() => {
-    return {
-      width: size || 100,
-      height: size || 100
-    }
-  }, [size])
+export const Avatar = ({
+  className,
+  src,
+  size = 100,
+  alt,
+  fallbackInverted
+}: AvatarProps) => {
+  const mods: Mods = {}
+
+  const styles = useMemo<CSSProperties>(() => ({
+    width: size,
+    height: size
+  }), [size])
+
+  const fallback = <Skeleton width={size} height={size} border="50%" />
+  const errorFallback = <Icon inverted={fallbackInverted} width={size} height={size} Svg={UserIcon} />
 
   return (
-    <img src={src} alt={alt} style={styles} className={classNames(cls.Avatar, mods, [className])}/>
+    <AppImage
+      fallback={fallback}
+      errorFallback={errorFallback}
+      src={src}
+      alt={alt}
+      style={styles}
+      className={classNames(cls.Avatar, mods, [className])}
+    />
   )
 }
