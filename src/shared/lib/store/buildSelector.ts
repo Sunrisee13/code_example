61945bf1741ac24d60826c8f7441edb305b0c069
ALeksandr Yurkovskiy <alus03@mail.ru>
2023-09-16 11:02:35 +0300

    124 Селекторы с аргументами. Типизация buildSelector

--- src/shared/lib/store/buildSelector.ts
+++ src/shared/lib/store/buildSelector.ts
@@ -1,14 +1,33 @@
+// import { useSelector } from 'react-redux'
+
+// import { type StateSchema } from '@/app/providers/StoreProvider'
+
+// type Selector<T> = (state: StateSchema) => T
+// type Result<T> = [() => T, Selector<T>]
+
+// export function buildSelector<T> (selector: Selector<T>): Result<T> {
+//   const useSelectorHook = () => {
+//     return useSelector(selector)
+//   }
+
+//   return [useSelectorHook, selector]
+// }
+
+// // --------------
+
 import { useSelector } from 'react-redux'
 
 import { type StateSchema } from '@/app/providers/StoreProvider'
 
-type Selector<T> = (state: StateSchema) => T
-type Result<T> = [() => T, Selector<T>]
+type Selector<T, Args extends any[]> = (state: StateSchema, ...args: Args) => T
+type Hook<T, Args extends any[]> = (...args: Args) => T
+type Result<T, Args extends any[]> = [Hook<T, Args>, Selector<T, Args>]
 
-export function buildSelector<T> (selector: Selector<T>): Result<T> {
-  const useSelectorHook = () => {
-    return useSelector(selector)
-  }
+export function buildSelector<T, Args extends any[]> (
+  selector: Selector<T, Args>
+): Result<T, Args> {
+  const useSelectorHook: Hook<T, Args> = (...args: Args) =>
+    useSelector((state: StateSchema) => selector(state, ...args))
 
   return [useSelectorHook, selector]
 }
