ba2060fd26165cc57796f8edbea2fca318f48f88
ALeksandr Yurkovskiy <alus03@mail.ru>
2023-09-16 11:25:40 +0300

    125.2 концепция Feature flags. Постепенное внедрение новых фичей

--- src/pages/ArcticleDetailsPage/ui/ArticleDetailsPage.tsx
+++ src/pages/ArcticleDetailsPage/ui/ArticleDetailsPage.tsx
@@ -7,6 +7,7 @@ import { ArticleRating } from '@/feature/articleRating'
 import { ArticleRecommendations } from '@/feature/articleRecommendations'
 import { classNames } from '@/shared/lib/classNames/classNames'
 import { DynamicModuleLoader, type ReducersList } from '@/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader'
+import { getFeatureFlag } from '@/shared/lib/features'
 import { VStack } from '@/shared/ui/Stack'
 import { Page } from '@/widgets/Page'
 
@@ -31,12 +32,18 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
     return null
   }
 
+  const isArticleRatingEnabled = getFeatureFlag('isArticleRatingEnabled')
+  const isCounterEnabled = getFeatureFlag('isCounterEnabled')
+
   return (
       <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
           <Page className={classNames(cls.ArticleDetailsPage, {}, [className])}>
               <VStack gap="16" max>
                   <ArticleDetailsPageHeader />
                   <ArticleDetails id={id} />
+                  {/* eslint-disable-next-line i18next/no-literal-string */}
+                  {isCounterEnabled && <div>Counter</div>}
+                  {isArticleRatingEnabled && <ArticleRating articleId={id} />}
                   <ArticleRating articleId={id} />
                   <ArticleRecommendations />
                   <ArticleDetailsComments id={id} />
